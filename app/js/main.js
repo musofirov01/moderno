e082520ef55c515c72327aa8a761c9b90ad3a612
musofirov01 <musofirov01@mail.ru>
2021-02-06 19:14:19 +0300

    added product-slider and product

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,20 @@
 $(function () {
     
 
+    $(".product-slider__inner").slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4
+    });
 
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: true
+    });
+
+    var mixer = mixitup('.product__inner-box');
 
 
 });
